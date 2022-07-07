811c895713c53c2b4d46d3e75a8c1ec86bca1822
Saad Ali <saad.ali@cogentlabs.co>
2022-07-07 12:20:52 +0500

    Class based generic views

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from users.views import (
     SignupView,
-    UserLoginView,
-    UserLogoutView
 )
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name="signup"),
-    path('login/', UserLoginView.as_view(), name="login"),
-    path('logout/', UserLogoutView.as_view(), name="logout")
+    path('login/', auth_views.LoginView.as_view(template_name='users/user_login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/user_login.html'), name="logout")
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,49 +1,9 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render, HttpResponseRedirect
-from django.views import View
+from django.views import generic
 
 from .forms import SignUpForm
 
 
-class SignupView(View):
-
-    def get(self, request):
-        form = SignUpForm()
-        context = {
-            'form': form
-        }
-        return render(request, 'users/signup.html', context)
-
-    def post(self, request):
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/login")
-
-
-class UserLoginView(View):
-
-    def get(self, request):
-        form = AuthenticationForm()
-        context = {
-            'form': form
-        }
-        return render(request, 'users/signup.html', context)
-
-    def post(self, request):
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            valid_user = authenticate(username=username, password=password)
-            if valid_user is not None:
-                login(request, valid_user)
-                return HttpResponseRedirect("/")
-
-
-class UserLogoutView(View):
-
-    def post(self, request):
-        logout(request)
-        return HttpResponseRedirect("/")
+class SignupView(generic.CreateView):
+    form_class = SignUpForm
+    success_url = '/'
+    template_name = 'users/signup.html'
