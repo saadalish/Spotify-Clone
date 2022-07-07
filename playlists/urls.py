3b0a416639cf1f9af6761840b479437fcbb2b003
Saad Ali <saad.ali@cogentlabs.co>
2022-07-07 12:07:51 +0500

    Class based generic views

--- playlists/urls.py
+++ playlists/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('create', CreatePlaylistView.as_view(), name='create_playlist'),
     path('<slug:pk>', UpdatePlaylistView.as_view(), name='update_playlist'),
     path('<slug:pk>/update', UpdatePlaylistDetailsView.as_view(), name='update_playlist_details'),
-    path('<slug:pk>/delete', DeletePlaylistView.as_view(), name='delete'),
+    path('<slug:pk>/delete', DeletePlaylistView.as_view(), name='delete_playlist'),
     path('<playlist_id>/<song_id>/add', AddSongToPlaylistView.as_view(), name='add_song_to_playlist'),
     path('<playlist_id>/<song_id>/remove', RemoveSongToPlaylistView.as_view(), name='remove_song_from_playlist')
 ]
