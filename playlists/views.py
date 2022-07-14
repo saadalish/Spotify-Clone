9626015526c868996e3e9002edcc3f77b39403c1
Saad Ali <saad.ali@cogentlabs.co>
2022-07-14 16:34:49 +0500

    Generic Views DRF

--- playlists/views.py
+++ playlists/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -9,44 +10,19 @@ from playlists.serializers import PlaylistSerializer
 from songs.models import Song
 
 
-class PlaylistView(APIView):
+class PlaylistView(generics.ListCreateAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
     permission_classes = [IsAuthenticated]
 
-    # list
-    def get(self, request):
-        playlists = Playlist.objects.filter(user=self.request.user)
-        serializer = PlaylistSerializer(playlists, many=True)
-        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
-
-    # create
-    def post(self, request):
-        serializer = PlaylistSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(user=self.request.user)
-            return JsonResponse(status=status.HTTP_201_CREATED, data=serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
-class PlaylistDetail(APIView):
-
-    # retrieve
-    def get(self, request, pk):
-        playlist = get_object_or_404(Playlist, id=pk)
-        serializer = PlaylistSerializer(playlist)
-        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
-
-    # update
-    def put(self, request, pk):
-        playlist = get_object_or_404(Playlist, id=pk)
-        serializer = PlaylistSerializer(playlist, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return JsonResponse(serializer.data)
-
-    # destroy
-    def delete(self, request, pk):
-        playlist = get_object_or_404(Playlist, id=pk)
-        playlist.delete()
-        return JsonResponse(status=status.HTTP_200_OK, data="Playlist deleted successfully", safe=False)
+class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class AddSongToPlaylistView(APIView):
