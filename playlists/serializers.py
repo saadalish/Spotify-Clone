40fa93a631624d7957ebfd84fb41f76d06f30f9c
Saad Ali <saad.ali@cogentlabs.co>
2022-07-15 11:30:33 +0500

    ViewSets DRF

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+
 from playlists.models import Playlist
+from songs.serializers import SongSerializer
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
+    songs = SongSerializer(many=True, read_only=True)
+
     class Meta:
         model = Playlist
         fields = ['id', 'name', 'songs']
-        depth = 2
+
