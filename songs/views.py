94ed869f2a08340739cd9ccb85d3fb475c03a72d
Saad Ali <saad.ali@cogentlabs.co>
2022-07-19 18:31:50 +0500

    Pagination Added Following changes are made in this commit: 1) PageNumberPagination added to ALbum Views

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Album
 from songs.models import Song
@@ -21,7 +22,8 @@ class SongDetail(generics.RetrieveUpdateDestroyAPIView):
 class AlbumList(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
 
 
 class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
